e0a2f60f4f1b39b5216225791dac69ea26100536
Lucas-ASouza <lucasalves-x@hotmail.com>
2022-12-13 11:11:19 -0300

    Created Repo

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import './App.css'
+import Card from "./layout/Card"
+
+import Form from "./form/Input.jsx"
+
+
+export default () =>
+
+    <div className="App">
+        <h1> desafio</h1>
+
+        <div className="Exercises">
+
+            <Card>
+                <Form></Form>
+            </Card>
+    
+        </div>
+
+    </div>
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './index.css'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+import App from './components/App'
+
+const divEl = document.getElementById('root')
+
+ReactDOM.render(<App/>, divEl)
