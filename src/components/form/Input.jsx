5d187a64da8b54dbc9710352eaad1103c046b9f6
Lucas-ASouza <lucasalves-x@hotmail.com>
2022-12-13 11:37:25 -0300

    fix: alternative approach

--- src/components/form/Input.jsx
+++ src/components/form/Input.jsx
@@ -4,50 +4,42 @@ export default props => {
 
    
 
-   /* let [name, setName] = useState('')
+    let [name, setName] = useState('')
     let [password, setPassword] = useState('')
-    let [email, setEmail] = useState('')*/
+    let [email, setEmail] = useState('')
 
-   /* function whenChange(e) {
+    function whenChange(e) {
         setName(e.target.value)
-        setPassword(e.target.value)
-        setEmail(e.target.value)
         
     }
 
     
     function whenChangeP(e){
+
+        setPassword(e.target.value)
         
     }
 
     function whenChangeE(e){
         
-
-    }*/
+        setEmail(e.target.value)
+    }
 
      /*Objetivo:
       Armazenar o que é digitado nos 3 inputs e ao clicar em salvar, dar um alert com esses dados */
 
-    /*Inicialmente eu tentei fazer uma função para setar o state para cada um dos inputs, e eu
-    tinha um problema em que qualquer alteração nos inputs já chamava a função do Display(responsável
-        pelo alert) mesmo que a chamada da função fosse no submit(basicamente dava um alert a cada
-            caracter que digitava)
-            
-            
-            Agora tentei salvar os 3 dados em um objeto, e trata-lo como um estado único, porém
-            ao digitar alguma coisa em qualquer input, aparentemente ele se torna undefined e state para
-            de atualizar*/
-    const [user, setUser] = useState({ username: '', password: '', email: ''});
+    /*const [user, setUser] = useState({ username: '', password: '', email: ''});
     
     function whenChange(e){
 
         setUser(e.target.value)
 
-    }
+    }*/
 
-    function Display(user){
+    function Display(){
 
-        return console.log(user)
+
+        return alert(name, password, email)
     }
 
 
@@ -55,14 +47,12 @@ export default props => {
         <div id="root">
           
             <form id="desafio"> 
-
-                <input  placeholder="Insira o nome"  value={user.username}  required type="text" onInput={whenChange} />
-                <input  placeholder="Insira a senha" value={user.password}  required type="password" onInput={whenChange} />
-                <input  placeholder="Insira o e-mail" value={user.email}  required type="email" onInput={whenChange} />
-                
-                <button onClick={Display(user)} form="desafio">salvar</button>
+                <input  placeholder="Insira o nome"  value={name}  required type="text" onChange={whenChange}/>
+                <input  placeholder="Insira a senha" value={password}  required type="password" onChange={whenChangeP}/>
+                <input  placeholder="Insira o e-mail" value={email}  required type="email" onChange={whenChangeE}/>
+              
             </form>
-  
+            <button onClick={Display()}>salvar</button>
         </div>
     )
 }
